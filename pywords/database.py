15cab294b2489f19b3e6dec6488544067dfaeee9
sjgallagher2 <sjgallagher2@gmail.com>
2022-06-09 18:13:46 -0400

    Update dictline and database

--- pywords/database.py
+++ pywords/database.py
@@ -9,4 +9,17 @@ db_conn = sqlite3.connect(db_fname)
 db_cursor = db_conn.cursor()
 
 # Now we can query
+db_cursor.execute("""SELECT dictline.dl_stem1_uvij,inflections.infl_ending_uvij 
+FROM dictline
+JOIN inflections ON dictline.dl_type = inflections.infl_type 
+    AND dictline.dl_variant = inflections.infl_variant
+    AND dictline.dl_pos = inflections.infl_pos
+WHERE dictline.dl_pos IS 'N' AND dictline.dl_stem1_uvij GLOB 'acro*'
+    AND inflections.infl_stem_id IS 1 AND inflections.infl_frequency IS 'A'
+;
+""")
+
+rows = db_cursor.fetchall()
+for row in rows:
+    print(row)
 
