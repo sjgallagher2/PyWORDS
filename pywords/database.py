629d332174a8ada79f55dbd22ce37b605c91b762
sjgallagher2 <sjgallagher2@gmail.com>
2022-06-12 13:35:56 -0400

    Experiment with database queries

--- pywords/database.py
+++ pywords/database.py
@@ -1,25 +1,39 @@
 import sqlite3
 
 db_fname = "data/database/words.db"
-
-# Create a connector to the database
 db_conn = sqlite3.connect(db_fname)
-
-# Create a cursor for executing sql
 db_cursor = db_conn.cursor()
 
-# Now we can query
-db_cursor.execute("""SELECT dictline.dl_stem1_uvij,inflections.infl_ending_uvij 
-FROM dictline
-JOIN inflections ON dictline.dl_type = inflections.infl_type 
-    AND dictline.dl_variant = inflections.infl_variant
-    AND dictline.dl_pos = inflections.infl_pos
-WHERE dictline.dl_pos IS 'N' AND dictline.dl_stem1_uvij GLOB 'acro*'
-    AND inflections.infl_stem_id IS 1 AND inflections.infl_frequency IS 'A'
-;
-""")
+def get_stemN_endings(cursor,stem,stemN):
+    """
+    Get endings available for dl_stemN (N=1,2,3,4) for word stem `stem`
+    """
+    if stemN not in [1,2,3,4]:
+        raise ValueError("Stem must be one of: 1,2,3,4")
+    #db_cursor.execute("""SELECT *
+    cursor.execute("""SELECT dictline.dl_id,dictline.dl_stem{0}_uvij,inflections.infl_id,inflections.infl_ending_uvij 
+    FROM dictline
+    JOIN inflections ON dictline.dl_type = inflections.infl_type 
+        AND dictline.dl_variant = inflections.infl_variant
+        AND dictline.dl_pos = inflections.infl_pos
+    WHERE dictline.dl_stem{0}_vi = '{1}' 
+        AND inflections.infl_stem_id IS {0} AND inflections.infl_frequency IS 'A'
+    ;
+    """.format(stemN,stem.replace('u','v').replace('j','i')))
+    stem_rows = cursor.fetchall()
+    return stem_rows
 
-rows = db_cursor.fetchall()
-for row in rows:
-    print(row)
+
+def get_possible_endings(cursor,stem):
+    stem1_endings = get_stemN_endings(cursor,stem,1)
+    stem2_endings = get_stemN_endings(cursor,stem,2)
+    stem3_endings = get_stemN_endings(cursor,stem,3)
+    stem4_endings = get_stemN_endings(cursor,stem,4)
+    stem_endings = stem1_endings + stem2_endings + stem3_endings + stem4_endings
+    #for row in stem_endings:
+    #    print(row)
+    return stem_endings
+
+#for i in range(1000):
+#    get_possible_endings(db_cursor,"cael")
 
